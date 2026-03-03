836a105de94dc44e034b57392ab76b7a53b32045
cat6424 <cat6424@gmail.com>
2026-03-03 12:10:35 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(".m_nav").hide();
+$(".m_btn").click(function(){
+ $(".m_nav").slideToggle() 
+})
+/*Toggle 기능
+slideToggle() -> slideUp<->slideDown
+fadeToggle() -> fadeIn()<->fadeOut
+Toggle() -> show()<->hide()
+toggleClass('클래스이름') -> addClass('클래스명')<->removeClass('클래스명')
+             클래스명은 앞에 .을 찍지않기
+*/
+
